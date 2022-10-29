7ee64f584020581c5c96894baff78b162d8cef89
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-10-29 19:16:34 -0300

    Adiciona pagina principal

---
+++ src/pages/Main/index.js
@@ -0,0 +1,37 @@
+import Candidate from "../../components/Candidate";
+import CountryPercentBar from "../../components/CountryPercentBar";
+import RegionPercentBar from "../../components/RegionPercentBar";
+import "./style.css";
+import liveIcon from "../../images/live.png";
+
+export default function Main() {
+  return (
+    <div>
+      <header>
+        <div id="logo">
+          <span>BR|News</span>
+          <img src={ liveIcon } alt='live icon' height='80px' />
+        </div>
+        <h1>APURAÇÃO DOS VOTOS SEGUNDO TURNO <br /> PRESIDÊNCIA</h1>
+      </header>
+      <article>
+        <CountryPercentBar />
+        <div id="candidates">
+          <Candidate name='LULA' />
+          <Candidate name='JAIR BOLSONARO' />
+        </div>
+        <div id="region">
+          <h3>Região</h3>
+          <div id="region_bars">
+            <RegionPercentBar region='norte' />
+            <RegionPercentBar region='nordeste'/>
+            <RegionPercentBar region='centro-oeste'/>
+            <RegionPercentBar region='sul'/>
+            <RegionPercentBar region='sudeste'/>
+          </div>
+        </div>
+      </article>
+      <footer></footer>
+    </div>
+  )
+}
